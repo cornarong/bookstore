4d75e45d8e024db1aeb2e3248a4fe59a927b5b2b
cornarong <cornarong@gmail.com>
2021-08-14 00:51:39 +0900

    layout 구성 및 mysql연동, jpa 데이터 호출 테스트

--- src/main/java/com/mytoy/bookstore/controller/BoardController.java
+++ src/main/java/com/mytoy/bookstore/controller/BoardController.java
@@ -1,15 +1,26 @@
 package com.mytoy.bookstore.controller;
 
+import com.mytoy.bookstore.model.Board;
+import com.mytoy.bookstore.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("list")
-    public String list(){
+    public String list(Model model){
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards",boards);
         return "board/list";
     }
 
