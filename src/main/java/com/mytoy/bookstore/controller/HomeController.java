c8203896e11cc60d397d0f3e6e4c56908efa366d
cornarong <cornarong@gmail.com>
2021-11-09 23:06:53 +0900

    commit

--- src/main/java/com/mytoy/bookstore/controller/HomeController.java
+++ src/main/java/com/mytoy/bookstore/controller/HomeController.java
@@ -1,13 +1,30 @@
 package com.mytoy.bookstore.controller;
 
+import com.mytoy.bookstore.dto.BookDto;
+import com.mytoy.bookstore.model.BookType;
+import com.mytoy.bookstore.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final BookService bookService;
+
     @GetMapping
-    public String index(){
+    public String index(Model model,
+                        @PageableDefault(size = 12) Pageable pageable,
+                        @RequestParam(defaultValue = "") String searchTerm){
+            Page<BookDto> bookDtoList = bookService.all(searchTerm, null, pageable);
+
+            model.addAttribute("bookDtoList", bookDtoList);
         return "index";
     }
 
